0ed14924e7375d208c3272b4abf7baac1c384ba3
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2017-08-18 00:54:39 -0700

    Camera motion in place

---
+++ ConwayGameOfLife/Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//	TODO: Should have some way to hook up the Grid to the Camera to initialize sizes
+public class CameraMotion : MonoBehaviour
+{
+	public Vector3 rotationSpeeds;
+	private float deltaTime;
+	
+	void Update ()
+	{
+		transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeeds.x);
+		transform.Rotate(-Vector3.up * Time.deltaTime * rotationSpeeds.y);
+		transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeeds.z);
+	}
+}
